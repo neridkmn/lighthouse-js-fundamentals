5b9c99bc97a4e1335d26ed6443d96746bd79a3e3
Neriman Dikmen <sanerneriman@gmail.com>
2023-04-17 14:59:36 -0400

    console, return, even

---
+++ even.js
@@ -0,0 +1,14 @@
+const isEven = function (num) {
+  return num % 2 === 0; 
+}
+const tenIsEven = isEven(10);
+const elevenIsEven = isEven(11);
+console.log(tenIsEven);
+console.log(elevenIsEven);
+
+
+const even = function (num) {
+  return num % 2 === 0; 
+}
+console.log(even(10));
+console.log(even(11));
